a305354fb7b557d1b16306ba416727f58240d8ff
Luiz Correia <lerc.luizrodrigues@gmail.com>
2023-06-10 09:59:00 -0300

    pop test image

--- knn/knn.py
+++ knn/knn.py
@@ -99,7 +99,6 @@ for i, dir in enumerate(diretorio):
     teste_imagem.append(images[5])
     teste_categoria.append(categoria)
     images.pop(5)
-    print(len(images))
     for img in images[:9]:
         pastas[i].append(img)
 
